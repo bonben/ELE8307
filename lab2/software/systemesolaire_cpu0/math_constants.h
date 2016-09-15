4c1d9e905bcd72c82235b9422f8a4b54b7a7c66c
Mathieu Léonardon <mathieu.leonardon@u-bordeaux.fr>
2016-09-15 15:44:34 -0400

    Initial commit
    Signed-off-by: Mathieu Léonardon <mathieu.leonardon@u-bordeaux.fr>

---
+++ lab2/software/systemesolaire_cpu0/math_constants.h
@@ -0,0 +1,7 @@
+#ifndef MATH_CONSTANTS_H
+#define MATH_CONSTANTS_H
+
+
+#define PI		3.141592653589793f		// omg vsc
+
+#endif // MATH_CONSTANTS_H
---
+++ lab2/software/systemesolaire_cpu0/ss_planet_drawBall_opt.s
@@ -0,0 +1,6 @@
+.section .text
+.global _planete_drawball_opt
+_planete_drawball_opt :
+
+ret
+.end
